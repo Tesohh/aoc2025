653221351d0741819bd5812d4b3ec50684154c5c
Simone Tesini <simozilla006@gmail.com>
2025-12-09 14:09:24 +0100

    rename top10

--- day8/src/part2.rs
+++ day8/src/part2.rs
@@ -8,9 +8,9 @@ pub fn part2(vectors: &[Vec3i]) -> isize {
     assert_eq!(product.len(), (vectors.len().pow(2) - vectors.len()) / 2);
 
     product.sort_by_key(|&(_, _, dist)| dist);
-    let top10 = product.iter().map(|&(a, b, _)| (a, b)).collect_vec();
+    let best_connections = product.iter().map(|&(a, b, _)| (a, b)).collect_vec();
 
-    let (_, connections) = crate::connect_boxes(&top10);
+    let (_, connections) = crate::connect_boxes(&best_connections);
 
     let last = connections.last().unwrap();
     last.0.x * last.1.x
